4de8701ab6660d38f9ad11abc6e85ce1043b84fb
Iryna Stefaniuk <irinagoyan@gmail.com>
2024-07-12 18:13:40 +0300

    make build

---
+++ index.3acd2961.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("ul").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".list-item__link");e.setAttribute("src",r)});
+//# sourceMappingURL=index.3acd2961.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const largeImg = document.querySelector("#largeImg");
+const galeryImg = document.querySelector("ul");
+galeryImg.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const newLargeImg = e.target.closest(".list-item__link");
+    largeImg.setAttribute("src", newLargeImg);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
